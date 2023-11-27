6bc01e0da0c60bd715e00b69eebb5a9f455ba7f6
m00zy <maxwellzhu123@gmail.com>
2023-11-27 19:22:25 +1300

    added int_hcamp_deco2012 and pyr_hcamp_deco2012 and incorporated into model_v1

---
+++ pyr_hcamp_deco2012.py
@@ -0,0 +1,11 @@
+#https://hippocampome.org/php/neuron_page.php?id=4000
+#https://www.jneurosci.org/content/32/10/3366?ijkey=214bab14703c98a27c572070390b1fff82a578c3&keytype2=tf_ipsecsha
+
+params = {
+    'E_L': -70, 
+    'V_reset': -70,
+    'V_th': -50,
+    'tau_m': 22.4,
+    't_ref': 2,
+    'C_m' : 307.7 #C_m = tau/R with R being 65MOhms
+}
