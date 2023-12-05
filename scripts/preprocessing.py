7c264dfafcf590ce6c2cc12930109915b5eb47a3
m00zy <maxwellzhu123@gmail.com>
2023-12-05 17:04:57 +1300

    added functionality for converting simulation results into suitable spike train format

---
+++ scripts/preprocessing.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def simulation_results_to_spike_trains(results, runtime):
+    #Input is a list of np arrays, each representing the times at which a neuron spiked
+    num_neurons = len(results)
+    spike_trains = np.zeros((num_neurons, runtime))
+    
+    for i in range(num_neurons):
+        spike_train = np.zeros(runtime)
+        for time in results[i]:
+            spike_train[int(time)] = 1.0
+        spike_trains[i] = spike_train
+    return spike_trains
