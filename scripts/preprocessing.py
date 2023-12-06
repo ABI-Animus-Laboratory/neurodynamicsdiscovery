fc6badbb5798bab255fc675a04a5b79def8be8a8
m00zy <maxwellzhu123@gmail.com>
2023-12-06 14:46:41 +1300

    small changes

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -3,6 +3,7 @@ import numpy as np
 
 def simulation_results_to_spike_trains(results, runtime):
     #Input is a list of np arrays, each representing the times at which a neuron spiked
+    #Output is an array of arrays, each of which is a spike train
     num_neurons = len(results)
     spike_trains = np.zeros((num_neurons, runtime))
     
