792d571cce43b3b706f4534a7dcb3d241fa33705
m00zy <maxwellzhu123@gmail.com>
2023-12-18 19:47:09 +1300

    implemented a method for identifying place cells

---
+++ scripts/neuron_classification.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+
+def remove_weak_neurons(spikes_array, threshold=5):
+    #Removes neurons from data that have total number of spikes under threshold
+    for i in range(np.size(spikes_array, axis=0)-1, -1, -1):
+        if np.sum(spikes_array[i]) <= threshold:
+            spikes_array = np.delete(spikes_array, i, axis=0) 
+    return spikes_array
+
+def match_frames(spikes_array, X_coords, Y_coords, num_frames):
+    #Matches the number of frames in the x and y coordinates from EzTrack and the spikes data
+    #Calcium imaging in 30fps and behavioural data in 15fps assumption
+    X_coords = np.vstack((X_coords, X_coords)).reshape(-1, order='F')[:num_frames]
+    Y_coords = np.vstack((Y_coords, Y_coords)).reshape(-1, order='F')[:num_frames]
+    return X_coords, Y_coords
+
+def get_spike_coords(spikes_array, ez_df):
+    #Gives an array of all the spike-time location coordinates for each neuron
+    num_neurons = np.size(spikes_array, axis=0)
+    num_frames = np.size(spikes_array, axis=1)
+    X_coords, Y_coords = ez_df['X'].values, ez_df['Y'].values
+    X_coords, Y_coords = match_frames(spikes_array, X_coords, Y_coords, num_frames)
+    spike_coords = []
+    for i in range(num_neurons):
+        coords = []
+        for j in range(num_frames):
+            if spikes_array[i][j] == 1:
+                coords.append((X_coords[j], Y_coords[j]))
+        spike_coords.append(coords)
+    return spike_coords
+
+def get_centroid(coords, lower_percentile=None, upper_percentile=None):
+    #Returns the centre coordinate from an array of coordinates
+    coords = np.array(coords)
+    if not lower_percentile:
+        lower_percentile = np.percentile(coords, 30)
+    if not upper_percentile:
+        upper_percentile = np.percentile(coords, 70)
+    middle_percentile = coords[(coords >= lower_percentile) & (coords <= upper_percentile)]
+    middle_percentile = middle_percentile.reshape(-1, 2)
+    return np.average(middle_percentile, axis=0)
+
+def categorize_neurons_box(spikes_coords, x_min, x_max, y_min, y_max, place_field_percentage=0.5, acceptance_percentage=0.5):
+    #Categorizes neurons into location-specific and non location-specific
+    location_specific = []
+    non_specific = []
+    open_w = x_max - x_min
+    open_h = y_max - y_min
+    p_field_half_w = place_field_percentage * open_w * 0.5
+    p_field_half_h = place_field_percentage * open_h * 0.5
+    for index, neuron_spikes in enumerate(spikes_coords):
+        centroid = get_centroid(neuron_spikes)
+        box_w_upper, box_w_lower = centroid[0] + p_field_half_w, centroid[0] - p_field_half_w
+        box_h_upper, box_h_lower = centroid[1] + p_field_half_h, centroid[1] - p_field_half_h
+        count = 0
+        for coord in neuron_spikes:
+            x, y = coord[0], coord[1]
+            if x <= box_w_upper and x >= box_w_lower and y <= box_h_upper and y >= box_h_lower:
+                count += 1
+        if count >= acceptance_percentage * len(neuron_spikes):
+            location_specific.append((index+1, neuron_spikes))
+        else:
+            non_specific.append((index+1, neuron_spikes))
+    return location_specific, non_specific
+
+
+        
+
+def assign_quadrant(row, x_mid, y_mid):
+    if row['X'] < x_mid and row['Y'] < y_mid:
+        return 'Top Left'
+    elif row['X'] > x_mid and row['Y'] < y_mid:
+        return 'Top Right'
+    elif row['X'] < x_mid and row['Y'] > y_mid:
+        return 'Bottom Left'
+    else:
+        return 'Bottom Right'
+    
+
