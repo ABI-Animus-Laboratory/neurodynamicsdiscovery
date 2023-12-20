3fb7ff3a2300c04c12a1a88dc92fdd24023d7d4f
m00zy <maxwellzhu123@gmail.com>
2023-12-20 17:52:30 +1300

    did some tidying up

--- scripts/neuron_classification.py
+++ scripts/neuron_classification.py
@@ -1,26 +1,21 @@
 import pandas as pd
 import numpy as np
-
-def remove_weak_neurons(spikes_array, threshold=5):
-    #Removes neurons from data that have total number of spikes under threshold
-    for i in range(np.size(spikes_array, axis=0)-1, -1, -1):
-        if np.sum(spikes_array[i]) <= threshold:
-            spikes_array = np.delete(spikes_array, i, axis=0) 
-    return spikes_array
+import math
 
 def match_frames(spikes_array, X_coords, Y_coords, num_frames):
-    #Matches the number of frames in the x and y coordinates from EzTrack and the spikes data
-    #Calcium imaging in 30fps and behavioural data in 15fps assumption
+    '''
+    Fixes issue where the frame rate of the calcium imaging is 30fps, twice that of the 15fps behavioural recording
+    '''
     X_coords = np.vstack((X_coords, X_coords)).reshape(-1, order='F')[:num_frames]
     Y_coords = np.vstack((Y_coords, Y_coords)).reshape(-1, order='F')[:num_frames]
     return X_coords, Y_coords
 
-def get_spike_coords(spikes_array, ez_df):
-    #Gives an array of all the spike-time location coordinates for each neuron
+def get_spike_coords(spikes_array, X_coords, Y_coords):
+    '''
+    Given an array of spike trains and the EzTrack behvaioural data, returns an array of spike coordinates for each neuron
+    '''
     num_neurons = np.size(spikes_array, axis=0)
     num_frames = np.size(spikes_array, axis=1)
-    X_coords, Y_coords = ez_df['X'].values, ez_df['Y'].values
-    X_coords, Y_coords = match_frames(spikes_array, X_coords, Y_coords, num_frames)
     spike_coords = []
     for i in range(num_neurons):
         coords = []
@@ -34,44 +29,42 @@ def get_centroid(coords, lower_percentile=None, upper_percentile=None):
     #Returns the centre coordinate from an array of coordinates
     coords = np.array(coords)
     if not lower_percentile:
-        lower_percentile = np.percentile(coords, 30)
+        lower_percentile = np.percentile(coords, 0)
     if not upper_percentile:
-        upper_percentile = np.percentile(coords, 70)
+        upper_percentile = np.percentile(coords, 100)
     middle_percentile = coords[(coords >= lower_percentile) & (coords <= upper_percentile)]
     middle_percentile = middle_percentile.reshape(-1, 2)
     return np.average(middle_percentile, axis=0)
 
-def categorize_neurons_box(spikes_coords, x_min, x_max, y_min, y_max, place_field_percentage=0.30, acceptance_percentage=0.90):
-    #Categorizes neurons into interneurons, silent cells and place cells
+def categorize_neurons_box(spikes_coords, x_min, x_max, pf_area=0.5, acceptance=0.7):
+    #Categorizes neurons into interneurons, silent cells and place cells using box method
     neuron_types = {'Interneuron':[], 'Silent':[], 'Place':[]}
     for i in range(len(spikes_coords)-1, -1, -1):
         if len(spikes_coords[i]) < 5:
-            coord = spikes_coords.pop(i)
-            neuron_types['Silent'].append([i, coord])
+            coords = spikes_coords.pop(i)
+            neuron_types['Silent'].append([i+1, coords])
         if len(spikes_coords[i]) > 275:
-            coord = spikes_coords.pop(i)
-            neuron_types['Interneuron'].append([i, coord])
+            coords = spikes_coords.pop(i)
+            neuron_types['Interneuron'].append([i+1, coord])
 
-    open_w = x_max - x_min
-    open_h = y_max - y_min
-    p_field_half_w = place_field_percentage * open_w * 2
-    p_field_half_h = place_field_percentage * open_h * 2
+    field_w = x_max - x_min
+    pf_radius = (math.sqrt(pf_area) * field_w) / 2
     for index, neuron_spikes in enumerate(spikes_coords):
         centroid = get_centroid(neuron_spikes)
-        box_w_upper, box_w_lower = centroid[0] + p_field_half_w, centroid[0] - p_field_half_w
-        box_h_upper, box_h_lower = centroid[1] + p_field_half_h, centroid[1] - p_field_half_h
+        box_top, box_bottom = centroid[0] + pf_radius, centroid[0] - pf_radius
+        box_left, box_right = centroid[1] + pf_radius, centroid[1] - pf_radius
         count = 0
         for coord in neuron_spikes:
             x, y = coord[0], coord[1]
-            if x <= box_w_upper and x >= box_w_lower and y <= box_h_upper and y >= box_h_lower:
+            if x <= box_top and x >= box_bottom and y <= box_left and y >= box_right:
                 count += 1
-        if count >= acceptance_percentage * len(neuron_spikes):
-            neuron_types['Place'].append([index, coord])
+        if count >= acceptance * len(neuron_spikes):
+            neuron_types['Place'].append([index+1, neuron_spikes])
         else:
-            if len(neuron_spikes) < 60:
-                neuron_types['Silent'].append([index, coord])
+            if len(neuron_spikes) < 50:
+                neuron_types['Silent'].append([index+1, neuron_spikes])
             else:
-                neuron_types['Interneuron'].append([index, coord])
+                neuron_types['Interneuron'].append([index+1, neuron_spikes])
     return neuron_types
 
     
