ee6ccb130e0c189ff254c8f2b1a617bcb2d6b1de
m00zy <maxwellzhu123@gmail.com>
2024-02-29 14:14:02 +1300

    checked spike data preprocessing added comments

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,18 +1,24 @@
 import numpy as np
 import nest
-from scripts import visualizations, initializations, experiments
+from scripts import visualizations
 
-def preprocess_spike_data(an_array):
-    #Removes spike residue and sets spike values to 1
-    for i in range(np.size(an_array, axis=0)):
-        for j in range(np.size(an_array[i], axis=0) -1, 0, -1):
-            if an_array[i][j] != 0 and an_array[i][j-1] != 0:
-                an_array[i][j] = 0
-            if an_array[i][j] != 0 and an_array[i][j-1] == 0:
-                an_array[i][j] = 1
-        if an_array[i][0] != 0:
-            an_array[i][0] = 1
-    return an_array
+def preprocess_spike_data(spike_trains):
+    '''
+    Function for preprocessing the raw spike train data
+    Removes spike residues by setting consecutive non-leading values to 0 and normalizes all non-zero values to 1
+    Input is 2d numpy array
+    Output is 2d numpy array
+    '''
+    
+    for i in range(np.size(spike_trains, axis=0)):
+        for j in range(np.size(spike_trains[i], axis=0) -1, 0, -1):
+            if spike_trains[i][j] != 0 and spike_trains[i][j-1] != 0:
+                spike_trains[i][j] = 0
+            if spike_trains[i][j] != 0 and spike_trains[i][j-1] == 0:
+                spike_trains[i][j] = 1
+        if spike_trains[i][0] != 0:
+            spike_trains[i][0] = 1
+    return spike_trains
     
 
                 
