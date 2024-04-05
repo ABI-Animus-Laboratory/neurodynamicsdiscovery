3ab7cb45b67726adcb53a1914c0568d47a259f37
m00zy <maxwellzhu123@gmail.com>
2024-04-05 18:06:05 +1300

    Finished implementation of calcium data preprocessing

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def preprocess_spike_data(self, spike_trains):
+def preprocess_spike_data(spike_trains):
     '''
     Function for preprocessing the raw spike train data
     Removes spike residues by setting consecutive non-leading values to 0 and normalizes all non-zero values to 1
@@ -20,35 +20,30 @@ def preprocess_spike_data(self, spike_trains):
             spike_trains[i][0] = 1
     return spike_trains
 
-def scale_calcium_data(calcium_traces, spike_trains, V_t, V_reset):
+def preprocess_calcium_data(calcium_traces, spike_trains, V_t, V_reset, save_figs=False, save_data=False):
+
+    '''
+    Function for preprocessing the raw calcium data so that it is compatible with the outputs from NEST
+    Normalises the range of the data to the action potential threshold and reset threshold, as well as the magnitudes of the APs
+    Output is 2 2d numpy arrays
+    '''
 
     calcium_traces, spike_trains = np.array(calcium_traces), np.array(spike_trains)
+    f_maxs = calcium_traces *  1
 
     for calcium in calcium_traces:
 
-        og_calcium = calcium * 1
-
+        original_calcium = calcium * 1
         f_max = calcium * 1
+
 
         index = np.where((calcium_traces == calcium).all(axis=1))[0][0]
 
         ts = np.nonzero(spike_trains[index])[0]
 
-        ###Test something
-        # new_ts = []
-        # for t in ts:
-        #     current = calcium[t]
-        #     while current < max(calcium[t:t+15]):
-        #         current = calcium[t+1] 
-        #         t += 1
-        #     new_ts.append(t)
-        
-        # print(new_ts)
-        ###Test something
-
         new_ts = []
         for t in ts:
-
+            
             current = calcium[t]
 
             if current > calcium[t-1] and current > calcium[t+1]:
@@ -62,13 +57,15 @@ def scale_calcium_data(calcium_traces, spike_trains, V_t, V_reset):
                 while t + 1 < np.size(calcium) and current < calcium[t+1]:
                     current = calcium[t+1]
                     t +=1
-                new_ts.append(t)
+                if current != 0:
+                    new_ts.append(t)
             
             else:
                 while current < calcium[t+1]:
                     current = calcium[t+1]
                     t +=1
-                new_ts.append(t)
+                if current != 0:
+                    new_ts.append(t)
 
         mask = np.zeros_like(f_max, dtype=bool)
         mask[new_ts] = True
@@ -99,19 +96,26 @@ def scale_calcium_data(calcium_traces, spike_trains, V_t, V_reset):
         for j in outliers:
             calcium[j] = -50
 
-        fig, axs = plt.subplots(1, 3, figsize=(24, 8))
-        axs[1].plot(f_max)
-        axs[1].set_title('Fmax')
-        axs[2].plot(calcium)
-        axs[2].set_title('Nosrmalized Calcium')
-        axs[0].plot(og_calcium)
-        axs[0].set_title('Original Calcium')
-        fig.suptitle(f'Neuron {index+1}', fontsize=20)
+        if save_figs:
+            fig, axs = plt.subplots(1, 3, figsize=(24, 8))
+            axs[1].plot(f_max)
+            axs[1].set_title('Fmax')
+            axs[2].plot(calcium)
+            axs[2].set_title('Normalized Calcium')
+            axs[0].plot(original_calcium)
+            axs[0].set_title('Original Calcium')
+            fig.suptitle(f'Neuron {index+1}', fontsize=20)
 
-        plt.savefig(f'/hpc/mzhu843/modelling/nest/plots/normalized calcium/Neuron {index+1}')
-        plt.close()
+            plt.savefig(f'/hpc/mzhu843/modelling/nest/plots/normalized calcium/Neuron {index+1}')
+            plt.close()
         
-    return calcium
+        calcium_traces[index] = calcium
+        f_maxs[index] = f_max
+
+    if save_data:
+        np.save('/hpc/mzhu843/modelling/nest/DATA/processed/calcium_traces/C_3_p', calcium_traces)
+        
+    return calcium_traces, f_maxs
     
     
 
