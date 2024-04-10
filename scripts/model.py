91ec95afefd29123d4e4719a4c4a63ddd5fa9e98
m00zy <maxwellzhu123@gmail.com>
2024-04-10 17:16:05 +1200

    some changes

--- scripts/model.py
+++ scripts/model.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 import nest
 from scripts import optimization
 from params import pyr_hcamp_deco2012, int_hcamp_deco2012
-import time
 
-class NeuronalNetwork:
+class Model:
     '''
     Class that represents the model
     '''
-    def __init__(self, weights, categorized_neurons, G_e = 3.7, G_i = -1, runtime = 17988, gamma_rate = 40, theta_rate = 7):
-        self.weights = weights
+    def __init__(self, categorized_neurons, G_e = 3.7, G_i = -1, runtime = 17988, gamma_rate = 40, theta_rate = 7):
+
         self.G_e = G_e
         self.G_i = G_i
         self.runtime = runtime
@@ -20,12 +19,12 @@ class NeuronalNetwork:
         self.resolution = 1
         self.V_e = 5
         self.V_i = 5
-        self.simulated = False
+        self.simulated = False             
 
         self.num_pyr = len(categorized_neurons['Place'])
         self.num_int = len(categorized_neurons['Interneuron'])
         self.num_ca1_neurons = self.num_pyr + self.num_int
-
+        
         self.spike_timings_pyr = None
         self.spike_trains_inter = None
 
@@ -35,91 +34,7 @@ class NeuronalNetwork:
         self.spike_recorder_pyr = None
         self.spike_recorder_int = None
 
-    def simulate(self):
-        '''
-        Runs a simulation and assigns class variables 
-        Takes in no inputs and has no output
-        '''
-        nest.ResetKernel()
-        
-        #Initialization of pyramimdal and interneurons
-        pyr = initialize_neuron_group('iaf_psc_alpha', self.num_pyr, pyr_hcamp_deco2012.params)
-        inter = initialize_neuron_group('iaf_psc_alpha', self.num_int, int_hcamp_deco2012.params)
 
-
-        #Initialization of EC neurons and connections
-        ec_input = nest.Create('poisson_generator')
-        ec_input.set(rate=self.gamma_rate)
-        ec_parrot = nest.Create('parrot_neuron', n=20)
-        nest.Connect(ec_input, ec_parrot)
-
-        #Initialization of CA3 neurons and connections
-        ca3_input = nest.Create('poisson_generator')
-        ca3_input.set(rate=self.gamma_rate)
-        ca3_parrot = nest.Create('parrot_neuron', n=20)
-        nest.Connect(ca3_input, ca3_parrot)
-
-        #Initialization of MS neurons and connections
-        ms_input = nest.Create('poisson_generator')
-        ms_input.set(rate=self.theta_rate)
-        ms_parrot = nest.Create('parrot_neuron', n=10)
-        nest.Connect(ms_input, ms_parrot)
-
-
-        #Initialization of spike recorders and connections for pyramidal and interneurons
-        spike_recorder_pyr = nest.Create('spike_recorder')
-        nest.Connect(pyr, spike_recorder_pyr)
-        spike_recorder_inter = nest.Create('spike_recorder')
-        nest.Connect(inter, spike_recorder_inter)
-
-        #Initialization of multimeter and connections for pyramidal and interneurons
-        multimeter_pyr = nest.Create('multimeter')
-        multimeter_pyr.set(record_from=["V_m"])
-        nest.Connect(multimeter_pyr, pyr)
-        multimeter_inter = nest.Create('multimeter')
-        multimeter_inter.set(record_from=["V_m"])
-        nest.Connect(multimeter_inter, inter)
-
-
-        #Initialization of connectivity weights
-        set_connection_weights(pyr, ec_parrot, ca3_parrot, inter, ms_parrot, self.weights, self.G_e, self.G_i, self.V_e, self.V_i,
-                               self.num_pyr, self.num_int)
-
-
-
-        #Run simulation
-        nest.Simulate(self.runtime)
-
-        self.simulated = True
-
-
-        #Accessing, processing and storing recorded place cell variables from simulation
-        spikes_pyr = nest.GetStatus(spike_recorder_pyr, "events")[0]
-        senders = spikes_pyr["senders"]
-        times = spikes_pyr["times"]
-        dmm_pyr = multimeter_pyr.get()
-        Vms_pyr = dmm_pyr["events"]["V_m"] 
-
-        spike_trains_pyr = [times[senders == neuron_id] for neuron_id in pyr]
-        spike_trains_pyr = simulation_results_to_spike_trains(spike_trains_pyr, self.runtime)
-        self.spike_timings_pyr = spike_trains_pyr
-        self.voltage_traces_pyr = tidy_Vms(Vms_pyr, self.num_pyr)
-        self.spike_recorder_pyr = spike_recorder_pyr
-
-
-        #Accessing, processing and storing recorded interneuron variables from simulation
-        spikes_int = nest.GetStatus(spike_recorder_inter, "events")[0]
-        senders = spikes_int["senders"]
-        times = spikes_int["times"]
-        dmm_int = multimeter_inter.get()
-        Vms_int = dmm_int["events"]["V_m"] 
-        spike_timings_int = [times[senders == neuron_id] for neuron_id in inter]
-        spike_timings_int = simulation_results_to_spike_trains(spike_timings_int, self.runtime)
-        self.spike_trains_int = spike_timings_int
-        self.voltage_traces_int = tidy_Vms(Vms_int, self.num_int)
-        self.spike_recorder_int = spike_recorder_inter
-
-    
     def check_simulated(self):
         '''
         Checks if a simulation has been run
@@ -166,8 +81,213 @@ class NeuronalNetwork:
             nest.raster_plot.from_device(self.spike_recorder_int)
         else:
             print("No simulation has been run!")
-    
-#Helper functions for the model class begin here
+
+class Model1(Model):
+    def __init__(self, categorized_neurons, weights = None, G_e = 3.7, G_i = -1, runtime = 17988, gamma_rate = 40, theta_rate = 7):
+
+        super().__init__(categorized_neurons, G_e, G_i, runtime, gamma_rate, theta_rate)
+        if weights is None:
+            self.weights = self.initialize_connectivity_matrix_normal_distribution()
+        else:
+            self.weights = weights
+
+    def simulate(self):
+        '''
+        Runs a simulation and assigns class variables 
+        Takes in no inputs and has no output
+        '''
+        nest.ResetKernel()
+        
+        #Initialization of pyramimdal and interneurons
+        pyr = initialize_neuron_group('iaf_psc_alpha', self.num_pyr, pyr_hcamp_deco2012.params)
+        inter = initialize_neuron_group('iaf_psc_alpha', self.num_int, int_hcamp_deco2012.params)
+
+        #Initialization of EC neurons and connections
+        ec_input = nest.Create('poisson_generator')
+        ec_input.set(rate=self.gamma_rate)
+        ec_parrot = nest.Create('parrot_neuron', n=20)
+        nest.Connect(ec_input, ec_parrot)
+
+        #Initialization of CA3 neurons and connections
+        ca3_input = nest.Create('poisson_generator')
+        ca3_input.set(rate=self.gamma_rate)
+        ca3_parrot = nest.Create('parrot_neuron', n=20)
+        nest.Connect(ca3_input, ca3_parrot)
+
+        #Initialization of MS neurons and connections
+        ms_input = nest.Create('poisson_generator')
+        ms_input.set(rate=self.theta_rate)
+        ms_parrot = nest.Create('parrot_neuron', n=10)
+        nest.Connect(ms_input, ms_parrot)
+
+
+        #Initialization of spike recorders and connections for pyramidal and interneurons
+        spike_recorder_pyr = nest.Create('spike_recorder')
+        nest.Connect(pyr, spike_recorder_pyr)
+        spike_recorder_inter = nest.Create('spike_recorder')
+        nest.Connect(inter, spike_recorder_inter)
+
+        #Initialization of multimeter and connections for pyramidal and interneurons
+        multimeter_pyr = nest.Create('multimeter')
+        multimeter_pyr.set(record_from=["V_m"])
+        nest.Connect(multimeter_pyr, pyr)
+        multimeter_inter = nest.Create('multimeter')
+        multimeter_inter.set(record_from=["V_m"])
+        nest.Connect(multimeter_inter, inter)
+
+
+        #Initialization of connectivity weights
+        self.set_connection_weights(pyr, ec_parrot, ca3_parrot, inter, ms_parrot, self.weights, self.G_e, self.G_i, self.V_e, self.V_i,
+                               self.num_pyr, self.num_int)
+
+        #Run simulation
+        nest.Simulate(self.runtime)
+
+        self.simulated = True
+
+        #Accessing, processing and storing recorded place cell variables from simulation
+        spikes_pyr = nest.GetStatus(spike_recorder_pyr, "events")[0]
+        senders = spikes_pyr["senders"]
+        times = spikes_pyr["times"]
+        dmm_pyr = multimeter_pyr.get()
+        Vms_pyr = dmm_pyr["events"]["V_m"] 
+
+        spike_trains_pyr = [times[senders == neuron_id] for neuron_id in pyr]
+        spike_trains_pyr = simulation_results_to_spike_trains(spike_trains_pyr, self.runtime)
+        self.spike_timings_pyr = spike_trains_pyr
+        self.voltage_traces_pyr = tidy_Vms(Vms_pyr, self.num_pyr)
+        self.spike_recorder_pyr = spike_recorder_pyr
+
+        #Accessing, processing and storing recorded interneuron variables from simulation
+        spikes_int = nest.GetStatus(spike_recorder_inter, "events")[0]
+        senders = spikes_int["senders"]
+        times = spikes_int["times"]
+        dmm_int = multimeter_inter.get()
+        Vms_int = dmm_int["events"]["V_m"] 
+        spike_timings_int = [times[senders == neuron_id] for neuron_id in inter]
+        spike_timings_int = simulation_results_to_spike_trains(spike_timings_int, self.runtime)
+        self.spike_trains_int = spike_timings_int
+        self.voltage_traces_int = tidy_Vms(Vms_int, self.num_int)
+        self.spike_recorder_int = spike_recorder_inter
+
+    def initialize_connectivity_matrix_normal_distribution(self):
+        '''
+        order and quantities
+        num_pyr pyramidal neurons
+        20 ec
+        20 ca3
+        num_int interneurons
+        10 medial septum
+
+        excitatory connections
+        pyr -> pyr
+        ca3 -> pyr, inter
+        ec -> pyr, inter
+
+        inh connections
+        int -> pyr
+        ms -> int
+        '''
+
+        num_pyr = len(self.categorised_neurons['Place'])
+        num_int = len(self.categorised_neurons['Interneuron'])
+        num_neurons = num_pyr + num_int + 50 #20 EC + 20 CA3 + 10 MS neurons = 50 total neurons
+
+        matrix = np.zeros((num_neurons, num_neurons))
+        #Pyramidal neurons
+        for i in range(num_pyr):
+            #Pyramidal to Pyramidal
+            matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
+        #EC
+        for i in range(num_pyr, num_pyr+20):
+            #EC to Pyramidal
+            matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
+            #EC to Interneurons
+            matrix[i][num_pyr+40:num_pyr+40+num_int] = np.abs(np.random.normal(1, scale=0.4, size=num_int))
+        #CA3
+        for i in range(num_pyr+20, num_pyr+40):
+            #CA3 to Pyramidal
+            matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
+            #CA3 to Interneurons
+            matrix[i][num_pyr+40:num_pyr+40+num_int] = np.abs(np.random.normal(1, scale=0.4, size=num_int))
+        #Interneurons
+        for i in range(num_pyr+40, num_pyr+40+num_int):
+            #Interneurons to Pyramidal
+            matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
+        #Medial Septum
+        for i in range(num_pyr+40+num_int, num_pyr+50+num_int):
+            #Medial Septum to Interneurons
+            matrix[i][num_pyr+40:num_pyr+40+num_int] = np.abs(np.random.normal(1, scale=0.4, size=num_int))
+
+        return matrix
+
+    def set_connection_weights(self, pyr, ec, ca3, inter, ms, weights, G_e, G_i, V_e, V_i, num_pyr, num_int):
+
+        '''
+        Sets all connection weightings
+        '''
+        
+        pyr_pyr_conns = weights[0:num_pyr, 0:num_pyr]
+        connect_weights(pyr, pyr, pyr_pyr_conns, G_e, V_e)
+
+        ec_pyr_conns = weights[num_pyr:num_pyr+20, 0:num_pyr]
+        connect_weights(ec, pyr, ec_pyr_conns, G_e, V_e)
+
+        ec_inter_conns = weights[num_pyr:num_pyr+20, num_pyr+40:num_pyr+40+num_int]
+        connect_weights(ec, inter, ec_inter_conns, G_e, V_e)
+
+        ca3_pyr_conns = weights[num_pyr+20:num_pyr+40, 0:num_pyr]
+        connect_weights(ca3, pyr, ca3_pyr_conns, G_e, V_e)
+
+        ca3_inter_conns = weights[num_pyr+20:num_pyr+40, num_pyr+40:num_pyr+40+num_int]
+        connect_weights(ca3, inter, ca3_inter_conns, G_e, V_e)
+
+        inter_pyr_conns = weights[num_pyr+40:num_pyr+40+num_int, 0:num_pyr]
+        connect_weights(inter, pyr, inter_pyr_conns, G_i, V_i)
+
+        ms_inter_conns = weights[num_pyr+40+num_int: num_pyr+50+num_int, num_pyr+40:num_pyr+40+num_int]
+        connect_weights(ms, inter, ms_inter_conns, G_i, V_i)
+
+class Model2(Model):
+    def __init__(self, categorized_neurons, spike_weights, weights = None, G_e = 3.7, G_i = -1, runtime = 3000, gamma_rate = 40, theta_rate = 7):
+        super().__init__(categorized_neurons, G_e, G_i, runtime, gamma_rate, theta_rate)
+        self.spike_weights = spike_weights
+        if weights is None:
+            self.weights = self.initialize_connectivity_matrix_normal_distribution()
+        else:
+            self.weights = weights
+
+    def simulate(self):
+        nest.ResetKernel()
+
+        pyr = initialize_neuron_group('iaf_psc_alpha', 5, pyr_hcamp_deco2012.params)
+
+        spike_times = [t for t in range(1, self.runtime+1)]
+        input1 = nest.Create("spike_generator", params={"spike_times": spike_times, "spike_weights": self.spike_weights[0]}, n=1)
+        input2 = nest.Create("spike_generator", params={"spike_times": spike_times, "spike_weights": self.spike_weights[1]}, n=1)
+        input3 = nest.Create("spike_generator", params={"spike_times": spike_times, "spike_weights": self.spike_weights[2]}, n=1)
+        input4 = nest.Create("spike_generator", params={"spike_times": spike_times "spike_weights": self.spike_weights[3]}, n=1)
+        input5 = nest.Create("spike_generator", params={"spike_times": spike_times, "spike_weights": self.spike_weights[4]}, n=1)
+
+    def set_connection_weights(self, pyr, input1, input2, input3, input4, input5):
+
+    def initialize_connectivity_matrix_normal_distribution(self):
+
+        matrix = np.zeros((10, 10))
+
+        for i in range(5):
+            #Pyramidal to Pyramidal
+            matrix[i][0:5] = np.abs(np.random.normal(1, scale=0.4, size=5))
+        
+        matrix[5][0] = np.abs(np.random.normal(1, scale=0.4, size=1))
+        matrix[6][1] = np.abs(np.random.normal(1, scale=0.4, size=1))
+        matrix[7][2] = np.abs(np.random.normal(1, scale=0.4, size=1))
+        matrix[8][3] = np.abs(np.random.normal(1, scale=0.4, size=1))
+        matrix[9][4] = np.abs(np.random.normal(1, scale=0.4, size=1))
+
+        return matrix
+
+#Helper functions for the model classes begin here
         
 def tidy_Vms(Vms, num_neurons):
     '''
@@ -205,43 +325,6 @@ def connect_weights(A, B, W, G, V):
     Connects all neurons in groups A and B according to weight matrix W with global scaling of G and voltage of V
     '''
     nest.Connect(A, B, 'all_to_all', syn_spec={'weight': np.transpose(W) * G * V})
-        
-def set_connection_weights(pyr, ec, ca3, inter, ms, weights, G_e, G_i, V_e, V_i, num_pyr, num_int):
-
-    '''
-    Sets all connection weightings
-    '''
-    
-    pyr_pyr_conns = weights[0:num_pyr, 0:num_pyr]
-    connect_weights(pyr, pyr, pyr_pyr_conns, G_e, V_e)
-
-    ec_pyr_conns = weights[num_pyr:num_pyr+20, 0:num_pyr]
-    connect_weights(ec, pyr, ec_pyr_conns, G_e, V_e)
-
-    ec_inter_conns = weights[num_pyr:num_pyr+20, num_pyr+40:num_pyr+40+num_int]
-    connect_weights(ec, inter, ec_inter_conns, G_e, V_e)
-
-    ca3_pyr_conns = weights[num_pyr+20:num_pyr+40, 0:num_pyr]
-    connect_weights(ca3, pyr, ca3_pyr_conns, G_e, V_e)
-
-    ca3_inter_conns = weights[num_pyr+20:num_pyr+40, num_pyr+40:num_pyr+40+num_int]
-    connect_weights(ca3, inter, ca3_inter_conns, G_e, V_e)
-
-    inter_pyr_conns = weights[num_pyr+40:num_pyr+40+num_int, 0:num_pyr]
-    connect_weights(inter, pyr, inter_pyr_conns, G_i, V_i)
-
-    ms_inter_conns = weights[num_pyr+40+num_int: num_pyr+50+num_int, num_pyr+40:num_pyr+40+num_int]
-    connect_weights(ms, inter, ms_inter_conns, G_i, V_i)
-
-# def initialize_neuron_group(type, n=1, params={}, initial_vm = None):
-#     neurons = nest.Create(type, n=n, params=params)
-#     if not initial_vm:
-#         Vth = neurons.get('V_th')[0]
-#         Vreset = neurons.get('V_reset')[0]
-#         neurons.set({"V_m": Vreset + nest.random.uniform(0.0, Vth-Vreset)})
-#     else:
-#         neurons.set({"V_m": initial_vm})
-#     return neurons
 
 def initialize_neuron_group(type, n=1, params={}):
     neurons = nest.Create(type, n=n, params=params)
@@ -253,5 +336,3 @@ def initialize_neuron_group(type, n=1, params={}):
         Vreset = neurons.get('V_reset')
     neurons.set({"V_m": Vreset})
     return neurons
-
-    
--- scripts/optimization.py
+++ scripts/optimization.py
@@ -13,7 +13,7 @@ class SimulatedAnnealing(Annealer):
 
     def energy(self):
         #Change this later
-        network = model.NeuronalNetwork(self.state, self.categorized_neurons, runtime=2501)
+        network = model.Model1(self.categorized_neurons, self.state)
         network.simulate()
         place_pred = network.get_voltage_traces('Place')
         int_pred = network.get_voltage_traces('Inter')
@@ -21,11 +21,11 @@ class SimulatedAnnealing(Annealer):
         return cost_function
     
     def move(self):
-        for i in range(4000):
+        for i in range(3000):
             x = np.random.randint(0, np.shape(self.state)[1])
             y = np.random.randint(0, np.shape(self.state)[0])
             if self.state[x][y] != 0:
-                self.state[x][y] = min(max(self.state[x][y] + np.random.uniform(-0.4, 0.4), 0.01), 3)    
+                self.state[x][y] = min(max(self.state[x][y] + np.random.uniform(-0.2, 0.2), 0.01), 3)    
         
 def ssd_with_l1(place_obs, place_pred, int_obs, int_pred, lamb, weights):
     '''
@@ -36,54 +36,5 @@ def ssd_with_l1(place_obs, place_pred, int_obs, int_pred, lamb, weights):
     l1_penalty = np.sum(lamb * np.abs(weights))
     return sum_squared_difference + l1_penalty
 
-def initialize_connectivity_matrix_normal_distribution(categorised_neurons):
-    '''
-    order and quantities
-    num_pyr pyramidal neurons
-    20 ec
-    20 ca3
-    num_int interneurons
-    10 medial septum
 
-    excitatory connections
-    pyr -> pyr
-    ca3 -> pyr, inter
-    ec -> pyr, inter
-
-    inh connections
-    int -> pyr
-    ms -> int
-    '''
-
-    num_pyr = len(categorised_neurons['Place'])
-    num_int = len(categorised_neurons['Interneuron'])
-    num_neurons = num_pyr + num_int + 50 #20 EC + 20 CA3 + 10 MS neurons = 50 total neurons
-
-    matrix = np.zeros((num_neurons, num_neurons))
-    #Pyramidal neurons
-    for i in range(num_pyr):
-        #Pyramidal to Pyramidal
-        matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
-    #EC
-    for i in range(num_pyr, num_pyr+20):
-        #EC to Pyramidal
-        matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
-        #EC to Interneurons
-        matrix[i][num_pyr+40:num_pyr+40+num_int] = np.abs(np.random.normal(1, scale=0.4, size=num_int))
-    #CA3
-    for i in range(num_pyr+20, num_pyr+40):
-        #CA3 to Pyramidal
-        matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
-        #CA3 to Interneurons
-        matrix[i][num_pyr+40:num_pyr+40+num_int] = np.abs(np.random.normal(1, scale=0.4, size=num_int))
-    #Interneurons
-    for i in range(num_pyr+40, num_pyr+40+num_int):
-        #Interneurons to Pyramidal
-        matrix[i][0:num_pyr] = np.abs(np.random.normal(1, scale=0.4, size=num_pyr))
-    #Medial Septum
-    for i in range(num_pyr+40+num_int, num_pyr+50+num_int):
-        #Medial Septum to Interneurons
-        matrix[i][num_pyr+40:num_pyr+40+num_int] = np.abs(np.random.normal(1, scale=0.4, size=num_int))
-
-    return matrix
     
